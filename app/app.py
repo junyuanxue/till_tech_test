704b6b764b33f1aa903925397fd334a8535e9ee2
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-30 23:16:32 +0100

    trying to load json menu

--- app/app.py
+++ app/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, json, request
+from order import Order
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
+
+@app.route("/items", methods=["POST"])
+def add_item():
+    print("before json url")
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "hipstercoffee.json")
+    print("after json url")
+    menu = json.loads(open(json_url))
+    print(menu)
+
+    item = request.form.get("item")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":

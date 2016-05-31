6a001f02c2cb04d78b36129960a646069e3efdcb
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-31 18:23:15 +0100

    flask

--- app/app.py
+++ app/app.py
@@ -1,24 +1,27 @@
 import os
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, redirect, url_for, session
 from order import Order
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
 @app.route("/")
 def index():
+    order = Order(load_menu())
+    session["order"] = "hello"
     return render_template("index.html")
 
 @app.route("/items", methods=["POST"])
 def add_item():
-    print("before json url")
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "hipstercoffee.json")
-    print("after json url")
-    menu = json.loads(open(json_url))
-    print(menu)
-
     item = request.form.get("item")
-    return render_template("index.html")
+    order.add_item(item)
+    return redirect(url_for("index"))
+
+
+def load_menu():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.abspath(SITE_ROOT + "/../hipstercoffee.json")
+    return json.load(open(json_url))
 
 
 if __name__ == "__main__":
